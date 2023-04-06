635fe005c36a56a546ef10e1ae5608d1f8cf986a
jdblaauw <122444664+jdblaauw@users.noreply.github.com>
2023-04-06 11:39:12 +0000

    changes according to styleguide

--- customers.sql
+++ customers.sql
@@ -1,7 +1,17 @@
-SELECT customer_id, name, email, created_at first_order_at, (SELECT COUNT(id) FROM  O1 WHERE O1.customer_id = O2.customer_id) as Number_of_orders
-FROM  O2
-LEFT JOIN  ON .id = O2.customer_id
-GROUP BY 1,2,3,4
-ORDER BY first_order_at
-LIMIT 6
+with customers as (
 
+    select 
+    customer_id, 
+    name, 
+    email, 
+    created_at first_order_at, 
+    (select COUNT(id) from `analytics-engineers-club.coffee_shop.orders` O1 where O1.customer_id = O2.customer_id) as Number_of_orders
+    from `analytics-engineers-club.coffee_shop.orders` O2
+    left join `analytics-engineers-club.coffee_shop.customers` on `analytics-engineers-club.coffee_shop.customers`.id = O2.customer_id
+
+    group by 1,2,3,4
+
+    order by first_order_at
+
+    limit 5
+                    )
