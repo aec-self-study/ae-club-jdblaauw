52226f8b7a895b2104ed9995f579d589f188dd40
jdblaauw <122444664+jdblaauw@users.noreply.github.com>
2023-04-18 12:56:12 +0000

    added staging views

---
+++ dbt_project_jelle/models/staging/github/stg_github_issue_merged.sql
@@ -0,0 +1,21 @@
+with source as (
+    select * from {{ source('github', 'issue_merged')}}
+),
+
+renamed as (
+    select
+        issue_id,
+        actor_id as merge_user_id,
+        
+        commit_sha,
+
+        -- timestamps
+        merged_at,
+
+
+        -- excluded columns
+        --_fivetran_synced,
+    FROM source
+)
+
+select * from renamed
---
+++ dbt_project_jelle/models/staging/github/stg_issues.sql
@@ -0,0 +1,26 @@
+with source as (
+    select * from {{ source('github', 'issue') }}
+),
+
+renamed as (
+    select
+        id as issue_id,
+        body,
+        closed_at,
+        created_at,
+        locked,
+        milestone_id,
+        number,
+        pull_request,
+        repository_id,
+        state,
+        title,
+        updated_at,
+        user_id,
+
+        --excluded 
+        _fivetran_synced
+    from source
+)
+
+select * from renamed
